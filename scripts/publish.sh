0ffbd91fb45462e350361766650d742e41144b41
Liu (Zuguang Liu) <liu2z2@mail.uc.edu>
2021-01-29 19:48:38 -0500

    test script again

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 git add .
-git commit -a -m "Update at ${1:-$(date +%Y-%m-%dT%H:%M:%S%z)}"
+git commit -a -m "${1:-Update at $(date +%Y-%m-%dT%H:%M:%S%z)}"
 git push -u origin pelican-src
 pelican content -o output -s pelicanconf.py
 ghp-import output -r origin -b main
