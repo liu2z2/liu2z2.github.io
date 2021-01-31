f3975cee58a4e84d5b91ca42c1365f766021ebf2
Liu (Zuguang Liu) <liu2z2@mail.uc.edu>
2021-01-31 08:58:22 -0500

    migration done

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-bash scripts/save.sh
+bash scripts/save.sh "${1:-Update at $(date +%Y-%m-%dT%H:%M:%S%z)}"
 bash scripts/build.sh
 ghp-import output -r origin -b main
 git push origin main
