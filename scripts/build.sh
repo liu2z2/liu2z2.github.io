a8cb7fd5da85bfe438c0a958e7d572f278aa3960
Liu (Zuguang Liu) <liu2z2@mail.uc.edu>
2021-05-17 07:56:02 -0400

    Save source using

--- scripts/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Build HTML from source content
-
-pelican content -o output -s publishconf.py
--- scripts/save.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Simply commit and push
-
-git add .
-git commit -a -m "${1:-Update at $(date +%Y-%m-%dT%H:%M:%S%z)}"
-git push -u origin pelican-src
